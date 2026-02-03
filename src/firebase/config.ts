85957d743c9b8699d51159b0e4adc5ab3a7c2830
MickTash Staal <mnstaal@gmail.com>
2026-02-03 02:07:44 +0000

    can you use teh database wth  teh cinfiguration   apiKey: "AIzaSyBQntOYe

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "scopesearch-ai-61831617-482e9",
-  "appId": "1:472403261651:web:48230c5711819cd39b7c26",
-  "apiKey": "AIzaSyASJQ3rRRsz_f0D_TRy4brlsbxK3cQWz8g",
-  "authDomain": "scopesearch-ai-61831617-482e9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "472403261651"
+  apiKey: "AIzaSyBQntOYepvLVplqVVoVnzq4j7CRYK3qDlo",
+  authDomain: "studio-6042316553-65e28.firebaseapp.com",
+  projectId: "studio-6042316553-65e28",
+  storageBucket: "studio-6042316553-65e28.firebasestorage.app",
+  messagingSenderId: "851458267599",
+  appId: "1:851458267599:web:4822e78bd7f9f498ca1aba",
+  measurementId: "G-L29MG32F1G"
 };
